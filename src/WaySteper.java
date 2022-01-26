bbfd3193b2e9031e74ba4c68b2ed4764db648ec2
PoLLaziY <xlanzerx@gmail.com>
2022-01-26 17:58:01 +0300

    Add LocationReader;

--- src/WaySteper.java
+++ src/WaySteper.java
@@ -1,10 +1,29 @@
+import java.io.IOException;
+
+import static java.lang.Thread.sleep;
+
 public class WaySteper {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
+        System.in.read();
+        new Thread(() -> {
+
         long start = System.currentTimeMillis();
         LocationReader l = new LocationReader("src/input.txt");
         Ways ways = new Ways(l.getLocation());
-        System.out.println(ways.getNumberOfWays());
-        System.out.println(ways.getResult());
-        System.out.println(System.currentTimeMillis() - start);
+            new Thread(() -> {
+                while (true) {
+                    System.out.println("Ways number is " + ways.getNumberOfWays());
+                    System.out.println("Best way if " + ways.getResult());
+                    try {
+                        sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+            ways.start();
+        }).start();
+
+
     }
 }
--- src/Ways.java
+++ src/Ways.java
@@ -3,47 +3,54 @@ import java.util.ArrayList;
 //Ways - хранилище всех возможных путей (Way) и методы по их созданию и движению
 public class Ways {
     //массив путей(Way)
-    private ArrayList<Way> ways = new ArrayList<Way>();
+    private final ArrayList<Way> ways = new ArrayList<Way>();
     //переданная локация в виде двумерного квадратного массива целочисленных значений
-    private int[][] loc;
+    private final int[][] loc;
+
+    private String resultWay;
 
     //конструктор Ways. Принимает локацию в виде двумерного квадратного массива целочисленных значений
     public Ways(int[][] loc) {
         this.loc = loc;
-        start();
     }
 
     //начало работы Ways. Создаёт 2 начальных пути - правый и нижний,
     //на которых вызывается метод для совершения следующего шага
-    private void start() {
+    public void start() {
         nextStep(new Way(loc[0][0]), 1, 0, 'D');
         nextStep(new Way(loc[0][0]), 0, 1, 'R');
     }
 
     //метод, совершающий следующий шаг, принимает путь(Way), который программа уже прошла, делает следующий шаг
     // и записывает его в путь. Если возможно два пути движения - правый путь создаёт копию пути (Way) и пишет в неё.
-    private void nextStep(Way way, int nextLine, int nextColumn, char turn){
+    private void nextStep(Way way, int nextLine, int nextColumn, char turn) {
         way.add(loc[nextLine][nextColumn], turn);
         if (nextLine == loc.length - 1 && nextColumn == loc[nextLine].length - 1) {
             way.safeToWays();
         } else if (nextLine == loc.length - 1) {
-            nextStep(way, nextLine, nextColumn+1, 'R');
+            nextStep(way, nextLine, nextColumn + 1, 'R');
         } else if (nextColumn == loc[nextLine].length - 1) {
-            nextStep(way, nextLine+1, nextColumn, 'D');
+            nextStep(way, nextLine + 1, nextColumn, 'D');
         } else {
-            nextStep(new Way(way), nextLine, nextColumn+1, 'R');
-            nextStep(way, nextLine+1, nextColumn, 'D');
+            nextStep(new Way(way), nextLine, nextColumn + 1, 'R');
+            nextStep(way, nextLine + 1, nextColumn, 'D');
         }
-    };
+    }
 
     public String getResult() {
-        int result = 0;
-        for (int i = 1; i < ways.size() - 1; i++) {
-            if (ways.get(result).SumResult < ways.get(i).SumResult) {
-                result = i;
+        synchronized (ways) {
+            if (ways.size() != 0) {
+                int result = 0;
+                for (int i = 1; i < ways.size() - 1; i++) {
+                    if (ways.get(result).SumResult < ways.get(i).SumResult) {
+                        result = i;
+                    }
+                }
+                resultWay = ways.get(result).toString();
+                ways.clear();
             }
+            return resultWay;
         }
-        return ways.get(result).toString();
     }
 
     public int getNumberOfWays() {
@@ -53,10 +60,10 @@ public class Ways {
     //Way - класс хранилище всех совершённых поворотов и пройденных результатов
     private class Way {
         private static int WayNumber = 0;
-        private ArrayList<Integer> results;
-        private ArrayList<Character> turns;
+        private final ArrayList<Integer> results;
+        private final ArrayList<Character> turns;
         private int SumResult;
-        private int myNumber;
+        private final int myNumber;
 
         //создание начального пути 1 стартовой точке
         private Way(int StartResults) {
@@ -83,7 +90,9 @@ public class Ways {
         }
 
         private void safeToWays() {
-            ways.add(this);
+            synchronized (ways) {
+                ways.add(this);
+            }
         }
 
         public String toString() {
